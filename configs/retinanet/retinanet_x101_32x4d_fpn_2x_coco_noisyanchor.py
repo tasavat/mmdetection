565a69f8adf09d5f9dd69ad60312165647abc2e8
tasavat <tasavattrisitichoke@gmail.com>
2022-02-22 22:40:28 +0900

    Noisy Anchor: fix bugs

--- configs/retinanet/retinanet_x101_32x4d_fpn_2x_coco_noisyanchor.py
+++ configs/retinanet/retinanet_x101_32x4d_fpn_2x_coco_noisyanchor.py
@@ -3,7 +3,9 @@ model = dict(
     bbox_head=dict(
         type='NoisyAnchorRetinaHead',
         max_n_bbox_per_gt=30,
-        alpha=0.75,
-        gamma=1.0,
+        cleanliness_alpha=0.75,
+        reweight_gamma=1.0,
+        focal_loss_alpha=0.25,
+        focal_loss_gamma=2.0,
     )
 )
